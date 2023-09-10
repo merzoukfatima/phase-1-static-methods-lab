338d470b013d9237906a596fc7676cff0e68af1d
MerzoukFatimaZohra <merzoukfatimazahraa@gmail.com>
2023-09-10 19:29:01 +0100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase().concat(string.slice(1))
+  }
+  static sanitize(string) {
+    //create a class with regex
+    return string.replace(/[^a-zA-Z-'\s]/g,"")
+  }
+  static titleize(string) {
+    //put all the except words in an array
+    const except = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    // split because we need array functions
+    return string.split(" ").map((element, index) => {
+      // if not found in except or  was the first word
+      return (except.indexOf(element) === -1 || index === 0) ? this.capitalize(element) : element
+      // capitalize it else leave it
+    }).join(" ") // when finish return to string
+    //:)
+  }
 }
